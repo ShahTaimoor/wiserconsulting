18a927c70c99453ae7debbe89626df52bc1aec0b
mirhamza123 <mirh7169@gmail.com>
2026-04-10 15:13:18 +0500

    feat: add VisaConsultation and StarButton components with supporting assets

--- client/.next/static/chunks/src_app_page_tsx_1db1bc2f._.js
+++ client/.next/static/chunks/src_app_page_tsx_1db1bc2f._.js
@@ -1,10 +1,11 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/src_9e6b8d55._.js",
+  "static/chunks/src_5ef04808._.js",
   "static/chunks/node_modules_next_15251bc2._.js",
   "static/chunks/node_modules_zod_v4_373df17a._.js",
-  "static/chunks/node_modules_lucide-react_dist_esm_icons_c9dc4a2c._.js"
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_deb9d63d._.js"
 ],
     source: "dynamic"
 });
