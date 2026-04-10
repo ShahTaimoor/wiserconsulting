fcef79fd16e6d865288b0f738a42833f093eea2a
Abdulsami1122 <samij7141@gmail.com>
2026-04-10 15:23:26 +0500

    Stop tracking .next directory

--- client/.next/static/chunks/src_app_services_page_tsx_1db1bc2f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_0e9f4335._.js",
-  "static/chunks/src_app_services_page_tsx_bd255872._.js"
-],
-    source: "dynamic"
-});
